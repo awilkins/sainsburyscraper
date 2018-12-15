ea1111d484e8a965b7f44716779c078b864a4d96
Adrian Wilkins <adrian.wilkins@dwp.gsi.gov.uk>
2018-12-15 22:27:33 +0000

    Implementation of title

--- src/main/java/uk/co/strangeowl/scraper/results/Result.java
+++ src/main/java/uk/co/strangeowl/scraper/results/Result.java
@@ -1,4 +1,14 @@
 package uk.co.strangeowl.scraper.results;
 
 public class Result {
+
+    private String title;
+
+    public Result(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
