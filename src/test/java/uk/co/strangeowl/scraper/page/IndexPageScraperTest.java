6e25fef62ff4aeebaf1092f672b52690d404ade1
Adrian Wilkins <adrian.wilkins@dwp.gsi.gov.uk>
2018-12-16 00:36:14 +0000

    Implemented tests
    * Must find product links
    * Must ignore cross-sell links

--- src/test/java/uk/co/strangeowl/scraper/page/IndexPageScraperTest.java
+++ src/test/java/uk/co/strangeowl/scraper/page/IndexPageScraperTest.java
@@ -7,18 +7,20 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.junit.Test;
 
+import javax.lang.model.util.Elements;
 import java.io.IOException;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.CoreMatchers.*;
 
 
-public class ScraperTest {
+public class IndexPageScraperTest {
 
     private static String getPage() {
         try {
             return Resources.toString(
-                    Resources.getResource(ScraperTest.class, "berries-cherries-currants6039.html"),
+                    Resources.getResource(IndexPageScraperTest.class, "berries-cherries-currants6039.html"),
                     Charsets.UTF_8
             );
         }
@@ -35,6 +37,23 @@ public class ScraperTest {
     public void canFindStrawberryProductLink() {
         Element firstLink = page.selectFirst(".product a");
         assertThat(firstLink.attr("href"), containsString("british-strawberries"));
+    }
+
+    @Test
+    public void canScrapeProductLinks() {
+        List<String> links = IndexPageScraper.productLinks(page);
+
+        // First link
+        assertThat(links, hasItem(containsString("british-strawberries")));
+
+        // Last link
+        assertThat(links, hasItem(containsString("cherry---strawberry-pack")));
+    }
+
+    @Test
+    public void avoidsCrossSellProducts() {
+        List<String> links = IndexPageScraper.productLinks(page);
+        assertThat(links, not(hasItem(containsString("klip-lock-storage"))));
     }
 
 
