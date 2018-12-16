091d027ba740231e5d101802f0a0d7fae5949654
Adrian Wilkins <adrian.wilkins@dwp.gsi.gov.uk>
2018-12-16 02:38:51 +0000

    Application is functional

--- src/main/java/uk/co/strangeowl/scraper/Application.java
+++ src/main/java/uk/co/strangeowl/scraper/Application.java
@@ -1,7 +1,74 @@
 package uk.co.strangeowl.scraper;
 
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import uk.co.strangeowl.scraper.page.IndexPageScraper;
+import uk.co.strangeowl.scraper.page.ProductPageScraper;
+import uk.co.strangeowl.scraper.results.Result;
+import uk.co.strangeowl.scraper.results.ResultSerializer;
+import uk.co.strangeowl.scraper.results.Results;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
 public class Application {
 
     public static final String TARGET_PAGE = "https://jsainsburyplc.github.io/serverside-test/site/www.sainsburys.co.uk/webapp/wcs/stores/servlet/gb/groceries/berries-cherries-currants6039.html";
 
+    private static Document getPage(String url) {
+        try {
+            return getPage(new URL(url));
+        }
+        catch (MalformedURLException malfex) {
+            System.out.println("Malformed URL : " + url);
+            return null;
+        }
+    }
+
+    private static Document getPage(URL url) {
+        try {
+            Document page = Jsoup.connect(url.toString()).get();
+            return page;
+        }
+        catch (IOException ioex) {
+            System.out.println("Failed to get page : " + url.toString());
+            return null;
+        }
+    }
+
+
+
+    private static void addResultFromUrl(String base, String url, Results results) {
+        try {
+            URL baseUrl = new URL(base);
+            URL productUrl = new URL(baseUrl, url);
+            Document productPage = getPage(productUrl.toString());
+            Result result = ProductPageScraper.scrape(productPage);
+            results.add(result);
+        }
+        catch (MalformedURLException malfex) {
+            System.out.println("Bad URL");
+            System.exit(1);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // Get the main page
+        Document indexPage = getPage(TARGET_PAGE);
+
+        List<String> productUrls = IndexPageScraper.productLinks(indexPage);
+
+        Results results = new Results();
+        for(String url : productUrls) {
+            addResultFromUrl(TARGET_PAGE, url, results);
+        }
+
+        System.out.print(
+                ResultSerializer.serialize(results)
+        );
+    }
 }
--- src/main/java/uk/co/strangeowl/scraper/page/ProductPageScraper.java
+++ src/main/java/uk/co/strangeowl/scraper/page/ProductPageScraper.java
@@ -1,8 +1,11 @@
 package uk.co.strangeowl.scraper.page;
 
+import com.google.common.annotations.VisibleForTesting;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import uk.co.strangeowl.scraper.results.Result;
+import uk.co.strangeowl.scraper.results.Results;
 
 import java.math.BigDecimal;
 
@@ -10,22 +13,34 @@ public class ProductPageScraper {
 
     final static String NON_NUMERIC = "[^0-9\\.]";
 
-    public static String getTitle(Document page) {
+    @VisibleForTesting
+    static String getTitle(Document page) {
         return page.selectFirst(".productSummary h1").text();
     }
 
-    public static String getDescription(Document page) {
-        return page.selectFirst(".productText p").text();
+    @VisibleForTesting
+    static String getDescription(Document page) {
+
+        Element description = page.select(".productText p")
+                .stream()
+                .filter(element -> element.text().length() > 0)
+                .findAny()
+                .orElse(null);
+
+        return description.text();
+
     }
 
-    public static BigDecimal getUnitPrice(Document page) {
+    @VisibleForTesting
+    static BigDecimal getUnitPrice(Document page) {
         String priceText = page.selectFirst(".pricePerUnit").text();
         // Remove all the non-numeric bits
         priceText = priceText.replaceAll(NON_NUMERIC, "");
         return new BigDecimal(priceText);
     }
 
-    public static Integer getKcalsPerHundredGrams(Document page) {
+    @VisibleForTesting
+    static Integer getKcalsPerHundredGrams(Document page) {
         Elements nutritionCells = page.select("tr[class*=\"tableRow\"] > td:first-of-type");
         Element kilocals = nutritionCells.stream()
                 .filter(element -> element.text().contains("kcal"))
@@ -35,5 +50,14 @@ public class ProductPageScraper {
         if (kilocals == null) return null;
 
         return Integer.valueOf(kilocals.text().replaceAll(NON_NUMERIC, ""));
+    }
+
+    public static Result scrape(Document page) {
+        return new Result(
+                getTitle(page),
+                getDescription(page),
+                getUnitPrice(page),
+                getKcalsPerHundredGrams(page)
+        );
     }
 }
