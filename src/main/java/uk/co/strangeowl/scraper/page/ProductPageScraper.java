d5d7954c5603a3613ba8e1e027592d36ba3e1d58
Adrian Wilkins <adrian.wilkins@dwp.gsi.gov.uk>
2018-12-16 00:49:39 +0000

    Implement getTitle method

--- src/main/java/uk/co/strangeowl/scraper/page/ProductPageScraper.java
+++ src/main/java/uk/co/strangeowl/scraper/page/ProductPageScraper.java
@@ -1,4 +1,11 @@
 package uk.co.strangeowl.scraper.page;
 
+import org.jsoup.nodes.Document;
+
 public class ProductPageScraper {
+
+    public static String getTitle(Document page) {
+        return page.selectFirst(".productSummary h1").text();
+    }
+
 }
